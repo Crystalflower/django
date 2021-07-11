0c37edcc793643fc3224f66dea203e53ef8bb51d
Froz <kolyamoi@mail.ru>
2021-07-11 16:08:57 +0300

    lesson 6 home work done

--- base_site/mainapp/urls.py
+++ base_site/mainapp/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import products
+from .views import product, products
 
 app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
     path('category/<int:pk>/', products, name='category'),
+    path('product/<int:pk>/', product, name='product'),
 ]
 
 if settings.DEBUG:
